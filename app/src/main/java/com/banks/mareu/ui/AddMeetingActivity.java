47081f10e415636893d49d6e89cb48b673adfa57
reneldev <reneldigital@gmail.com>
2022-01-10 23:57:55 +0100

    Implementation DatePicker - AddMeetingActivity

--- app/src/main/java/com/banks/mareu/ui/AddMeetingActivity.java
+++ app/src/main/java/com/banks/mareu/ui/AddMeetingActivity.java
@@ -2,15 +2,20 @@ package com.banks.mareu.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.Toast;
 
 import com.banks.mareu.R;
 import com.banks.mareu.databinding.ActivityAddMeetingBinding;
 import com.banks.mareu.model.Room;
+
+import java.util.Calendar;
 
 public class AddMeetingActivity extends AppCompatActivity {
 
@@ -18,13 +23,14 @@ public class AddMeetingActivity extends AppCompatActivity {
     ArrayAdapter<Room> mAdapter;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= ActivityAddMeetingBinding.inflate(getLayoutInflater());
+        binding = ActivityAddMeetingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mAdapter = new ArrayAdapter<Room>(this, R.layout.room_item,Room.values());
+        mAdapter = new ArrayAdapter<Room>(this, R.layout.room_item, Room.values());
         binding.autoComplete.setAdapter(mAdapter);
 
         binding.autoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -32,13 +38,30 @@ public class AddMeetingActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String item = adapterView.getItemAtPosition(i).toString();
                 Toast.makeText(getApplicationContext(), item, Toast.LENGTH_SHORT).show();
-
             }
         });
 
+        binding.editText1.setInputType(InputType.TYPE_NULL);
 
-
-
+        binding.editText1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                DatePickerDialog picker;
+                picker = new DatePickerDialog(AddMeetingActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                binding.editText1.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
 
     }
 }
