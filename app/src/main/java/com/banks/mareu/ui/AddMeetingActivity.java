2608460d280b413c07b54be8d4367c31e2668cde
reneldev <reneldigital@gmail.com>
2022-01-10 23:21:46 +0100

    Implementation Exposed Drop-Down Menu (choose room)

--- app/src/main/java/com/banks/mareu/ui/AddMeetingActivity.java
+++ app/src/main/java/com/banks/mareu/ui/AddMeetingActivity.java
@@ -3,16 +3,42 @@ package com.banks.mareu.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.banks.mareu.R;
+import com.banks.mareu.databinding.ActivityAddMeetingBinding;
 import com.banks.mareu.model.Room;
 
 public class AddMeetingActivity extends AppCompatActivity {
 
+    ActivityAddMeetingBinding binding;
+    ArrayAdapter<Room> mAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_meeting);
-        Room.values();
+        binding= ActivityAddMeetingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAdapter = new ArrayAdapter<Room>(this, R.layout.room_item,Room.values());
+        binding.autoComplete.setAdapter(mAdapter);
+
+        binding.autoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(getApplicationContext(), item, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+
+
     }
 }
